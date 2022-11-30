c7d3eb2375b920f7f2c8da8e3279a28a4882c12c
JimboBaggins <james.anderson98@outlook.com>
2022-11-30 17:51:08 +0000

    Add function to get computer choice

--- myjavascript.js
+++ myjavascript.js
@@ -5,3 +5,14 @@ let playerChoice = 0;
 let computerChoice = 0;
 let computerScore = 0;
 let playerScore = 0;
+
+//Get computer choice
+function getComputerChoice(compChoice) {
+  if (compChoice === 1) {
+    return "rock";
+  } else if (compChoice === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
